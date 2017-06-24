5666318f3a14b39facb3e55871c0ded8a46d5b7e
Moss Pakhapoca <thorsleepless@gmail.com>
2017-06-24 01:47:26 -0400

    Added more comments to solution 2

--- euler/euler12.py
+++ euler/euler12.py
@@ -7,6 +7,9 @@ import common
 def trigNum(n):
     return (n*(n+1))//2
 
+# Most triangular numbers (except for perfect square) has even numbers of divisors
+# Which means that we only have to count the "first half" hence count+2
+# Note: this is not the most optimal solution, but it is not too terribly slow either
 def run(lim):
     _lim = lim-1
     candidate = 0
@@ -18,16 +21,6 @@ def run(lim):
         candidate = trigNum(i)
         i+=1
 
-        # Second attempt is much faster
-        n = common.root(candidate)
-        for j in range(1, n+1):
-            if candidate%j == 0:
-                count+=2
-        if n**2 == candidate:
-            count-=1
-        if count > lim:
-            s = True
-
         # First attempt is very slow since we are trying to factor every possible number
         # if candidate%_lim == 0:
         #     count+=1
@@ -37,7 +30,19 @@ def run(lim):
         #         if count == _lim:
         #             s = True
         #             break
-    return count
+
+        # Second attempt is much faster
+        n = common.root(candidate)
+        for j in range(1, n+1):
+            if candidate%j == 0:
+                count+=2
+        # Correction for if the answer is perfect square
+        if n**2 == candidate:
+            count-=1
+        if count > lim:
+            s = True
+
+    return candidate
 
 if __name__ == '__main__':
     print("{}".format(run(500)))
